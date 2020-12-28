da1791763c5b4c2dbf9ea864c3499582fc14da18
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2020-12-28 13:05:47 +0530

    Adding source lines for some zsh plugins in zshrc

--- tools/install_zsh_plugins.sh
+++ tools/install_zsh_plugins.sh
@@ -4,6 +4,7 @@ zsh_plugins=(
   autojump
   zsh-autosuggestions
   zsh-syntax-highlighting
+  zsh-completions
   tree
   ack
   tig
@@ -13,7 +14,6 @@ zsh_plugins=(
   tldr
   tmux
   stern
-  zsh-completions
   fzf
 )
 
@@ -21,6 +21,12 @@ e_header "Installing zsh helper plugins.."
 for i in ${zsh_plugins[@]}; do
   brew install $i
 done
+
+# Activating plugins by sourcing in zshrc
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
+echo "[ -f $(brew --prefix)/etc/profile.d/autojump.sh ] && . $(brew --prefix)/etc/profile.d/autojump.sh" >>~/.zshrc
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>~/.zshrc
+echo "export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=$(brew --prefix)/share/zsh-syntax-highlighting/highlighters" >>~/.zshenv
 
 #install forgit. (This requires fzf installed)
 #This changes zshrc
